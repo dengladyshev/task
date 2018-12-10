69a0f3a6fa9a21da2d45bc7f2b438ede830ae496
Denis Gladyshev <d.gladyshev@lamoda.ru>
2018-12-10 22:59:00 +0300

    Initital

---
+++ Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Gladyshev Denis Vladimirovich on 10/12/2018.
+//  Copyright © 2018 dengladyshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
